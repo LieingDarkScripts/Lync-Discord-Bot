713fee1080774d03267c3f271b1c0b35d3e5cd64
LieingDarkScripts <59669511+LieingDarkScripts@users.noreply.github.com>
2022-04-12 16:52:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Programmed by Ben. S. Created on 4/12/2022; 3:58 PM
 
 const Discord = require("discord.js");
-const {MessageEmbed} = Discord;
-
 const {LyncsToken} = process.env; // Heroku configuration variables
 
 const Client = new Discord.Client({
