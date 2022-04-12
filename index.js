6894df165aa13c2fcf9542dcb76468b458fd13f5
LieingDarkScripts <59669511+LieingDarkScripts@users.noreply.github.com>
2022-04-12 16:15:14 -0500

    Update index.js
    Added basic bot script to test out bot, still have not reached the goals functionality.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Programmed by Ben. S. Created on 4/12/2022; 3:58 PM
+
+const Discord = require("discord.js");
+const {MessageEmbed} = Discord;
+
+const {LyncsToken} = process.env; // Heroku configuration variables
+
+const Client = new Discord.Client({
+    intents: ["GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_EMOJIS_AND_STICKERS",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+        "GUILD_PRESENCES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING",
+        "GUILD_SCHEDULED_EVENTS",
+    ]
+});
+
+Client.on("messageCreate", (Message) => {
+
+    const {content, reply, bot} = Message;
+
+    if (bot) {
+       return;
+    }
+
+    reply(content.length);
+
+});
