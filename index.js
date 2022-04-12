aaa5d409c02f238c7d031a495355f8dd7f4bfe74
LieingDarkScripts <59669511+LieingDarkScripts@users.noreply.github.com>
2022-04-12 16:45:58 -0500

    intents no. 2

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ const {MessageEmbed} = Discord;
 
 const {LyncsToken} = process.env; // Heroku configuration variables
 
-const Client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] })
+const Client = new Discord.Client({
+    intents: ["GUILDS",
+        "GUILD_MEMBERS",
+        "GUILD_BANS",
+        "GUILD_EMOJIS_AND_STICKERS",
+        "GUILD_INTEGRATIONS",
+        "GUILD_WEBHOOKS",
+        "GUILD_INVITES",
+        "GUILD_VOICE_STATES",
+        "GUILD_PRESENCES",
+        "GUILD_MESSAGES",
+        "GUILD_MESSAGE_REACTIONS",
+        "GUILD_MESSAGE_TYPING",
+        "DIRECT_MESSAGES",
+        "DIRECT_MESSAGE_REACTIONS",
+        "DIRECT_MESSAGE_TYPING",
+        "GUILD_SCHEDULED_EVENTS",
+    ]
+});
 
-Client.login(LyncsToken)
+Client.login(Ly)
 
 Client.on("messageCreate", (Message) => {
 
