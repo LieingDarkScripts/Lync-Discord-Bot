265fa64017977b8889912b96d58e9fa5c42537da
LieingDarkScripts <59669511+LieingDarkScripts@users.noreply.github.com>
2022-04-12 17:37:59 -0500

    pasted example

--- index.js
+++ index.js
@@ -32,6 +32,19 @@ const CommandBuilderRest = new REST({ version: '9'}).setToken(LyncsToken)
 
 // Command creation
 
+const commands = [
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+]
+	.map(command => command.toJSON());
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+rest.put(Routes.applicationGuildCommands(ClientId, GuildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
+
 console.log("1")
 
 const LynkCommands = [
